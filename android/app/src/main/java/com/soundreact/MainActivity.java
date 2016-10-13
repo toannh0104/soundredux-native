3c92dc0d63d930caa07222a605e5748f40825fe3
ToanNH <toan_h@mitrais.com>
2016-10-13 18:06:02 +0800

    First react sound

--- android/app/src/main/java/com/soundreact/MainActivity.java
+++ android/app/src/main/java/com/soundreact/MainActivity.java
@@ -1,4 +1,4 @@
-package com.soundreduxnative;
+package com.soundreact;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -9,7 +9,6 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactRootView;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
 
 import com.xeodou.rctplayer.*;
 import com.oblador.vectoricons.VectorIconsPackage;
